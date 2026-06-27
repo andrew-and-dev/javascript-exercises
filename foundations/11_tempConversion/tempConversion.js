ca355eb099789e93dd0339c847ee30aeaa3e2f0d
Andrei Kartashov <kartashovandry@gmail.com>
2026-06-27 06:56:37 -0400

    Finish exersice 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (tempInFahrenheit) {
+  let tempInCelsius = (tempInFahrenheit - 32) * (5 / 9);
+
+  function roundToOneDecimal(value) {
+    return Math.round((value + Number.EPSILON) * 10) / 10;
+  }
+
+  return roundToOneDecimal(tempInCelsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (tempInCelsius) {
+  let tempToFahrenheit = tempInCelsius * (9 / 5) + 32;
+
+  function roundToOneDecimal(value) {
+    return Math.round((value + Number.EPSILON) * 10) / 10;
+  }
+
+  return roundToOneDecimal(tempToFahrenheit);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
